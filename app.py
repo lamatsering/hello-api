7ce8c3b04071ada400879ec0ab669757a37d3d16
Tsering Lama <101940935+lamatsering@users.noreply.github.com>
2025-09-05 19:20:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return "I’m alive"
+    return "I am June Lama,here to rule the world"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
